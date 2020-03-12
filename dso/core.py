c7fa289cedc9b15d34a2efda15583ca02f275755
Arnaut <44735497+ArnautS@users.noreply.github.com>
2020-03-12 15:43:01 +0100

    Group target and reference functions together

--- dso/core.py
+++ dso/core.py
@@ -3,35 +3,43 @@ from sqlalchemy.orm import sessionmaker
 from structure import RoadSectionRef, JunctionRef, RoadSectionTarget, JunctionTarget, DelimitedStrokeRef, DelimitedStrokeTarget
 from helpers import classify_junctions, construct_strokes, reset_delimited_strokes, construct_stroke_from_section
 
+
+def run_reference(preprocessing_check):
+    session.query(DelimitedStrokeRef).delete()
+    reset_delimited_strokes(session.query(RoadSectionRef))
+
+    junctions_ref = session.query(JunctionRef)
+    if preprocessing_check:
+        classify_junctions(junctions_ref, session)
+
+    print("constructing strokes reference database")
+    construct_strokes(junctions_ref, session, DelimitedStrokeRef)
+    remaining_sections_target = session.query(RoadSectionTarget).filter(RoadSectionTarget.delimited_stroke_id == None)
+    for road_section in remaining_sections_target:
+        construct_stroke_from_section(road_section, session, DelimitedStrokeTarget)
+
+
+def run_target(preprocessing_check):
+    session.query(DelimitedStrokeTarget).delete()
+    reset_delimited_strokes(session.query(RoadSectionTarget))
+
+    junctions_target = session.query(JunctionTarget)
+    if preprocessing_check:
+        classify_junctions(junctions_target, session)
+
+    print("constructing strokes target database")
+    construct_strokes(junctions_target, session, DelimitedStrokeTarget)
+    remaining_sections_ref = session.query(RoadSectionRef).filter(RoadSectionRef.delimited_stroke_id == None)
+    for road_section in remaining_sections_ref:
+        construct_stroke_from_section(road_section, session, DelimitedStrokeRef)
+
+
 # create a connection with the database holding the road_section, junction and delimited_stroke tables
 engine = create_engine('postgresql://postgres:admin@localhost/postgis_sample')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-junctions_ref = session.query(JunctionRef)
-junctions_target = session.query(JunctionTarget)
-
-# reset previously generated strokes
-
-reset_delimited_strokes(session.query(RoadSectionRef))
-
-session.query(DelimitedStrokeTarget).delete()
-reset_delimited_strokes(session.query(RoadSectionTarget))
-
-# classify_junctions(junctions_ref, session)
-# classify_junctions(junctions_target, session)
-
-# creates a delimited stroke for every road_section in the input table
-# construct_strokes(junctions_ref, session)
-construct_strokes(junctions_target, session)
-
-remaining_sections_ref = session.query(RoadSectionRef).filter(RoadSectionRef.delimited_stroke_id == None)
-for road_section in remaining_sections_ref:
-    construct_stroke_from_section(road_section)
-
-remaining_sections_target = session.query(RoadSectionTarget).filter(RoadSectionTarget.delimited_stroke_id == None)
-for road_section in remaining_sections_target:
-    construct_stroke_from_section(road_section)
-
+run_reference(False)
+run_target(False)
 
 session.commit()
