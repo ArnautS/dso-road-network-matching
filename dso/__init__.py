612fb4587ccf406e6593e4bd7113e3b2eab095ec
Arnaut <arnautscholten1996@gmail.com>
2020-04-15 16:28:45 +0200

    Update initial variables

--- dso/__init__.py
+++ dso/__init__.py
@@ -2,8 +2,10 @@ from math import pi
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-deviation_angle = pi / 18  # radians
+deviation_angle = 20  # degrees
+deviation_angle = deviation_angle * pi / 180  # radians
 tolerance_distance = 20  # meters
+match_id = 0
 
 engine = create_engine('postgresql://postgres:admin@localhost/postgres')
 Session = sessionmaker(bind=engine)
