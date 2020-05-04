69a9eced44e8eac6df916825e9b074ea43830889
Arnaut <arnautscholten1996@gmail.com>
2020-05-04 10:54:00 +0200

    Reformat matching execution from core

--- dso/core.py
+++ dso/core.py
@@ -1,6 +1,4 @@
-from sqlalchemy.orm import lazyload
-
-from dso import session
+from dso import session, delimited_strokes
 from structure import RoadSectionRef, JunctionRef, RoadSectionTarget, JunctionTarget, DelimitedStrokeRef, \
     DelimitedStrokeTarget
 from helpers import classify_junctions, construct_strokes, reset_delimited_strokes, construct_stroke_from_section, \
@@ -9,7 +7,7 @@ from matching import find_matching_candidates
 from sqlalchemy.sql import func
 
 
-def process_reference(preprocessing_check):
+def preprocess_reference(preprocessing_check):
     session.query(DelimitedStrokeRef).delete()
     reset_delimited_strokes(session.query(RoadSectionRef))
 
@@ -24,7 +22,7 @@ def process_reference(preprocessing_check):
         construct_stroke_from_section(road_section, DelimitedStrokeRef)
 
 
-def process_target(preprocessing_check):
+def preprocess_target(preprocessing_check):
     session.query(DelimitedStrokeTarget).delete()
     reset_delimited_strokes(session.query(RoadSectionTarget))
 
@@ -39,37 +37,76 @@ def process_target(preprocessing_check):
         construct_stroke_from_section(road_section, DelimitedStrokeTarget)
 
 
-process_reference(0)
-process_target(0)
+def prepare_strokes_lvl2(delimited_stroke_class):
+    not_matched_strokes = session.query(delimited_stroke_class).filter(delimited_stroke_class.match_id == None)
+    for delimited_stroke in not_matched_strokes:
+        for road_section in delimited_strokes[delimited_stroke.id]:
+            road_section.delimited_stroke = None
+        begin_junction = delimited_stroke.begin_junction
+        for road_section in delimited_strokes[delimited_stroke.id]:
+            if road_section.delimited_stroke is None:
+                new_stroke = construct_stroke_from_section(road_section, delimited_stroke_class, level=2,
+                                                           begin_junction=begin_junction)
+                extended_stroke = construct_stroke(road_section, begin_junction, new_stroke, level=2)
+                begin_junction = extended_stroke.end_junction
 
-# strokes_ref = {}
-strokes_ref = session.query(DelimitedStrokeRef).order_by(DelimitedStrokeRef.id)
+    session.query(delimited_stroke_class).filter(delimited_stroke_class.level == 1,
+                                                 delimited_stroke_class.match_id == None).delete()
+
+
+def matching_process(level):
+    strokes_ref = session.query(DelimitedStrokeRef).filter(DelimitedStrokeRef.level == level)
+    strokes_target = session.query(DelimitedStrokeTarget).filter(DelimitedStrokeTarget.level == level)
+
+    reset_matches(strokes_ref)
+    reset_matches(strokes_target)
+
+    all_matches = []
+    for stroke_ref in strokes_ref:
+        matches = find_matching_candidates(stroke_ref)
+        all_matches.append(matches)
+        print(' ')
+    print('-----------------------------------------------------------')
+    for matches in all_matches:
+        if len(matches) > 1:
+            for match in matches:
+                print('Match:', match.id)
+                for stroke in match.strokes_ref:
+                    print('reference stroke', stroke.id)
+                for stroke in match.strokes_target:
+                    print('target stroke', stroke.id)
+            print(' ')
+
+
+road_sections_ref = {}
+for section in session.query(RoadSectionRef):
+    road_sections_ref[section.id] = section
+
+# for section in road_sections_ref:
+#     print(road_sections_ref[section].delimited_stroke_id) #, ' = ',
+#           strokes_ref[road_sections_ref[section].delimited_stroke_id].id)
+
 # , func.st_length(DelimitedStrokeRef.geom).label('length'))
 # for stroke, length in strokes_ref_query:
 #     stroke.length = length
 # add this to query extra attributes ^
-strokes_target = session.query(DelimitedStrokeTarget)
 
-# for each in strokes_ref:
-#     print(each.length)
 
-reset_matches(strokes_ref)
-reset_matches(strokes_target)
+preprocess_reference(0)
+preprocess_target(0)
 
-all_matches = []
-for stroke_ref in strokes_ref:
-    matches = find_matching_candidates(stroke_ref)
-    all_matches.append(matches)
-    print(' ')
-print('-----------------------------------------------------------')
-for matches in all_matches:
-    if len(matches) > 1:
-        for match in matches:
-            print('Match:', match.id)
-            for stroke in match.strokes_ref:
-                print('reference stroke', stroke.id)
-            for stroke in match.strokes_target:
-                print('target stroke', stroke.id)
-        print(' ')
+matching_process(1)
+
+prepare_strokes_lvl2(DelimitedStrokeRef)
+prepare_strokes_lvl2(DelimitedStrokeTarget)
+
+matching_process(2)
+
+# for stroke in delimited_strokes:
+#     print(stroke)
+#     for section in delimited_strokes[stroke]:
+#         print(section.id)
+
+
 session.commit()
 session.close()
