0da304e3e25d5cde93fb047000620597b41af12c
Arnaut <arnautscholten1996@gmail.com>
2020-06-16 18:42:54 +0200

    Refactor stroke construction functions to construction.py

--- dso/execution time.py
+++ dso/execution time.py
@@ -1,6 +1,7 @@
 from dso import session
 from structure import RoadSectionRef, JunctionRef, RoadSectionTarget, JunctionTarget, DelimitedStrokeRef, DelimitedStrokeTarget
-from helpers import classify_junctions, construct_strokes, reset_delimited_strokes, construct_stroke_from_section, construct_stroke
+from construction import classify_junctions, construct_stroke, construct_strokes, reset_delimited_strokes, \
+    construct_stroke_from_section
 from matching import find_matching_candidates
 
 junctions = session.query(JunctionRef)
