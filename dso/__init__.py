f2de485a988cd13d905d1ce05d06aca336891133
Arnaut <arnautscholten1996@gmail.com>
2020-06-16 14:58:51 +0200

    Add tolerance values

--- dso/__init__.py
+++ dso/__init__.py
@@ -4,9 +4,15 @@ from sqlalchemy.orm import sessionmaker
 
 deviation_angle = 20  # degrees
 deviation_angle = deviation_angle * pi / 180  # radians
-tolerance_distance = 20  # meters
 match_id = 0
 delimited_strokes = {}
+
+# tolerance values
+tolerance_distance = 20  # meters
+tolerance_length = 20  # meters
+tolerance_hausdorff = 20  # meters
+tolerance_area_normalized = 1.5  # ratio of length to area difference
+
 
 engine = create_engine('postgresql://postgres:admin@localhost/postgres')
 Session = sessionmaker(bind=engine)
